32741c63fde3e9f37f8b58380fa6b23a76524615
ali-saeed91 <engineer.ali91@live.com>
2022-08-26 03:03:06 +0500

    Updated Project Files

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const port = 4300;
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/api/v1/test/", function (req, res){
-    console.log('Test API')
+app.get("/api/v1/test/",function (req, res) {
+    console.log("Test API")
     res.send("Node CRUD NeDB App is Running")
 });
 
-// Import students.js module
+// Import students.js module 
 app.use('/api/v1/students/', students);
-app.listen(port, ()=> {console.log('Node Server Started')});
+app.listen(port, () => {console.log('Node Server Started')});
--- students.js
+++ students.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
+
+const CONNECTION_URL = 'mongodb+srv://mern:123@cluster0.gvlpgfx.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL)
+.then(() => console.log('Connected to Db'))
+.catch((error) => console.log(error.message));
+
+const client = new MongoClient(CONNECTION_URL);
+const studentDb = client.db('students');
+const studentsCol = studentDb.collection('studentsCol')
 
 // Connecting to the database
-var Datastore = require('nedb');
-var students = new Datastore ( {filename: 'database/students.db', autoload: true});
+// var Datastore = require('nedb');
+// var students = new Datastore ( {filename: 'database/students.db', autoload: true});
+
 
 // GET all students data from the database
 // Endpoint: /api/v1/students/
 
-router.get('/', async(req, res)=>{
+router.get('/', async(req,res)=>{
+    try{
+        // await studentsCol.find({}).exec(function(err, data){
+        //     if (err){
+        //         return res.status(500).json({
+        //             message: "Error" + err
+        //         })
+        //     }
+        //     res.send(data)
+        // })
+        const students = await studentsCol.find({});
+        console.log(students)
+        res.send('Working')
+    }
+    catch(error){
+        res.status(500).json({message: "Error in this function" + error})
+    }
+})
 
-    try {
-        await students.find({}).exec(function(err, data){
+// GET one students data by ID from the database
+// Endpoint: /api/v1/students/
+
+router.get('/:id', async(req,res)=>{
+    try{
+        await students.findOne({_id: req.params.id}, (err, data)=>{
             if (err){
                 return res.status(500).json({
                     message: "Error" + err
                 })
             }
-            res.send(data)
+            if(data){
+                return res.send(data)
+            }
+            res.status(404).json({message:"Student with this ID does not exist"})
         })
     }
     catch(error){
@@ -26,56 +63,122 @@ router.get('/', async(req, res)=>{
 })
 
 // POST student data to the database
-// Endpoint: /api/v1/students;
-
-router.post('/',async(req, res)=>{
-
-    try {
-        await students.insert(req.body, (err, addedData)=>{
+// Endpoint: /api/v1/students/
+router.post('/', async(req,res)=>{
+    try{
+        req.body.studentEmail = (req.body.studentEmail).toLowerCase();
+        await students.findOne({studentEmail: req.body.studentEmail}, (err, dataFound) => {
             if (err){
                 return res.status(500).json({
                     message: "Error" + err
                 })
-        }
-        console.log('inserted record', addedData);
-        return res.json({
-            message: "Student added succesfully",
-            studentId: addedData._id
+            }
+            console.log(dataFound)
+            if(dataFound==null){
+                students.insert(req.body, (err, addedData)=>{
+                    if (err){
+                        return res.status(500).json({
+                            message: "Error" + err
+                        })
+                    }
+                    console.log('inserted record', addedData);
+                    return res.json({
+                        message: "Student added successfully",
+                        studentId: addedData._id
+                    })
+                })
+
+            }
+            else{
+                res.status(500).json({message:"Student with this email already exist"})
+            }
+   
         })
-    })        
     }
     catch(error){
         res.status(500).json({message: "Error in this function" + error})
     }
-}) 
-
-// DELETE one student from database
+})
+// Update one student from the database
 // Endpoint: /api/v1/students/studentId
 
-router.delete ('/:id', async(req,res)=>{
-
-    try {
-        await students.remove( {_id: req.params.id},(err, deletedData)=>{
+router.patch('/:id', async(req, res) =>{
+    try{
+        await students.update({_id: req.params.id}, req.body, {upsert: false}, (err, updatedData)=>{
             if (err){
                 return res.status(500).json({
                     message: "Error" + err
                 })
-        }
-        //console.log(deletedData) for getting 0,1 status code for del or not del
-        if(deletedData){
-        
-            return res.json( {
-                message: "Student removed succesfully"
-            })
-        }
-        res.status(404).json({
-            message: "Student with this Id does not exist"
+            }
+            if(updatedData){
+                return res.json({
+                    message: "Student updated successfully"
+                })
+            }
+            res.status(404).json({message:"Student with this ID does not exist"})
         })
+    }
+    catch{
+        res.status(500).json({message: "Error in this function" + error})
+    }
+});
+
+// DELETE all students from the database
+// TODO: Remove from the code
+// Endpoint: /api/v1/students/
+
+router.delete ('/delete-all', async(req, res) =>{
+    console.log("delete all method")
+    try{
+        await students.remove({}, {multi:true}, (err, deletedData)=>{
+            if (err){
+                return res.status(500).json({
+                    message: "Error" + err
+                })
+            }
+            if(deletedData){
+                return res.json({
+                    message: "All student removed"
+                })
+            }
+            else{
+                return res.json({
+                    message: "No student removed"
+                })
+            }
+            
         })
     }
     catch(error){
         res.status(500).json({message: "Error in this function" + error})
     }
-}) 
+    
+})
+
+// DELETE one student from the database
+// Endpoint: /api/v1/students/studentId
+
+router.delete ('/:id', async(req, res) =>{
+    try{
+        await students.remove( {_id: req.params.id}, (err, deletedData)=>{
+            if (err){
+                return res.status(500).json({
+                    message: "Error" + err
+                })
+            }
+            if(deletedData){
+                return res.json({
+                    message: "Student removed successfully"
+                })
+            }
+            res.status(404).json({message:"Student with this ID does not exist"})
+        })
+    }
+    catch(error){
+        res.status(500).json({message: "Error in this function" + error})
+    }
+    
+})
+
 
 module.exports = router;
